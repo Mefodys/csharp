aab3aebb63060db38e1fb9291025b3a039fe05bf
Anton Mefodys <mefody123@gmail.com>
2021-11-05 08:24:24 +0300

    2commit

---
+++ first/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Mefody
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
